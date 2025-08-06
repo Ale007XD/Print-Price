44f28bfc07d706110958dacf90cc1f20154cb1cf
Ale007XD <alex.deloverov@gmail.com>
2025-08-06 09:27:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// Этот файл: sw.js
-const CACHE_NAME = 'banner-calculator-cache-v9';
+const CACHE_NAME = 'banner-calculator-cache-v11';
 const URLS_TO_CACHE = [
   './',
   './index.html',
@@ -16,34 +15,28 @@ const URLS_TO_CACHE = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Кэш открыт, добавляю файлы в кэш...');
-        return cache.addAll(URLS_TO_CACHE);
-      })
+      .then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
 
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
+    caches.keys().then(cacheNames =>
+      Promise.all(
         cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            console.log('Удаляю старый кэш:', cacheName);
             return caches.delete(cacheName);
           }
         })
-      );
-    })
+      )
+    )
   );
 });
